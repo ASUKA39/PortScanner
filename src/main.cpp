28beb895fb3791ba4e300ad7d378e81a1b7d7ba5
ASUKA39 <573470041@qq.com>
2024-03-15 23:57:43 +0800

    finish TCPConnect mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
-#include "threadpool.h"
+#include <vector>
 
-int main() {
-    int threadNum = 16;
+#include "threadpool.h"
+#include "scanner.h"
+
+int main(int argc, char* argv[]) {
+    int threadNum = 48;
+
+    if (argc < 2 || argc > 4) {
+        std::cerr << "Usage: " << argv[0] << " <ip>" << " <start port>" << " <end port>" << std::endl;
+        return 1;
+    }
+    std::string ip = argv[1];
+    int startPort = std::stoi(argv[2]);
+    int endPort = std::stoi(argv[3]);
+    std::vector<int> ports;
+    for (int i = startPort; i <= endPort; i++) {
+        ports.push_back(i);
+    }
+
     ThreadPool pool(threadNum);
     for (int i = 0; i < threadNum; i++) {
-        pool.enqueue([i] {
-            std::cout << "Task " << i << " is running on core " << sched_getcpu() << std::endl;
+        // 分段扫描
+        std::vector<int> subPorts(ports.begin() + i * (endPort - startPort) / threadNum, ports.begin() + (i + 1) * (endPort - startPort) / threadNum);
+        pool.enqueue([i, ip, subPorts] {
+            // std::cout << "Task " << i << " is running on core " << sched_getcpu() << std::endl;
+            ScannerFactory factory;
+            Scanner* scanner = factory.createScanner("TCPConnect");
+            scanner->scan(ip, subPorts);
         });
     }
     return 0;
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,30 +1,50 @@
+#include <arpa/inet.h>
 #include <iostream>
+#include <netinet/in.h>
+#include <unistd.h>
 #include <vector>
+#include <sys/socket.h>
 
 #include "scanner.h"
 
 void TCPConnectScanner::scan(const std::string& ip, const std::vector<int>& ports) {
-
+    // 与目标端口建立连接，如果连接成功则端口开放，收到RST则端口关闭
+    for (int port : ports) {
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            std::cerr << "Error: cannot open socket" << std::endl;
+            return;
+        }
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
+        if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+            // std::cout << "Port " << port << " is closed" << std::endl;
+        } else {
+            std::cout << "Port " << port << " is open" << std::endl;
+        }
+        close(sockfd);
+    }
 }
 
 void TCPSYNScanner::scan(const std::string& ip, const std::vector<int>& ports) {
-
+    // 发送SYN包，收到SYN/ACK则端口开放，收到RST则端口关闭
 }
 
 void TCPNULLScanner::scan(const std::string& ip, const std::vector<int>& ports) {
-
+    // 发送不带标志位的TCP包，如果没有回复则端口开放，收到RST则端口关闭
 }
 
 void TCPFINScanner::scan(const std::string& ip, const std::vector<int>& ports) {
-
+    // 发送FIN包，收到RST则端口关闭，收到FIN/ACK则端口开放
 }
 
 void UDPScanner::scan(const std::string& ip, const std::vector<int>& ports) {
-
+    // 发送UDP包，收到ICMP Port Unreachable则端口关闭，否则端口开放
 }
 
 void ICMPScanner::scan(const std::string& ip, const std::vector<int>& ports) {
-    
 }
 
 Scanner* ScannerFactory::createScanner(const std::string& type) {
