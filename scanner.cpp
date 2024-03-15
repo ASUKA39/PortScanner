bb7090a95c079968dc87083969ddddb5c3145874
ASUKA39 <573470041@qq.com>
2024-03-15 16:26:44 +0800

    first commit

---
+++ scanner.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <algorithm>
+#include <atomic>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+class ThreadPool {
+    
+};
+
+class Scanner {
+    
+}
+
+class TCP_connect : public Scanner {
+
+}
+
+class UDP : public Scanner {
+
+}
+
+class TCP_SYN : public Scanner {
+
+}
+
+class TCP_FIN : public Scanner {
+
+}
+
+int main(int argc, char* argv[]) {
+
+}
